479fc182c0c0b962dd4cdb1b9d7c9716bbe39a35
BAHTI <salah-eddine.bahti@capgemini.com>
2018-05-28 12:54:57 +0000

    delete test component imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValidateurComponent } from './Validateur/validateur.component';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ValidateurComponent
+    ValidateurComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
