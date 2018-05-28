8357556aa9f2fbe1dac9717b4c2ba5575a8728f7
BAHTI <salah-eddine.bahti@capgemini.com>
2018-05-28 13:06:59 +0000

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValidateurComponent } from './Validateur/validateur.component';
 import { TestComponent } from './test/test.component';
+import { HomeComponent } from './home/home.component';
+import { CollaborateurComponent } from './collaborateur/collaborateur.component';
+import { AdminComponent } from './admin/admin.component';
+import { LogInComponent } from './auth/log-in/log-in.component';
+import { SignInComponent } from './auth/sign-in/sign-in.component';
+import { DemandeComponent } from './demande/demande.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidateurComponent,
-    TestComponent
+    TestComponent,
+    HomeComponent,
+    CollaborateurComponent,
+    AdminComponent,
+    LogInComponent,
+    SignInComponent,
+    DemandeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/collaborateur/collaborateur.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-collaborateur',
+  templateUrl: './collaborateur.component.html',
+  styleUrls: ['./collaborateur.component.css']
+})
+export class CollaborateurComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
