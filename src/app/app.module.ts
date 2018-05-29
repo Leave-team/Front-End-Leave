cbbf4779cfdd314474ce7c7513ec011e41bc1c1c
medbrini <30552485+medbrini93@users.noreply.github.com>
2018-05-29 12:52:11 +0000

    Install package / AngularMaterials / Config and remove testComponent import call

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,19 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ValidateurComponent } from './Validateur/validateur.component';
-import { TestComponent } from './test/test.component';
 import { HomeComponent } from './home/home.component';
 import { CollaborateurComponent } from './collaborateur/collaborateur.component';
 import { AdminComponent } from './admin/admin.component';
 import { LogInComponent } from './auth/log-in/log-in.component';
 import { SignInComponent } from './auth/sign-in/sign-in.component';
 import { DemandeComponent } from './demande/demande.component';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     ValidateurComponent,
-    TestComponent,
     HomeComponent,
     CollaborateurComponent,
     AdminComponent,
@@ -26,7 +26,8 @@ import { DemandeComponent } from './demande/demande.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
