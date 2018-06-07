63bec95f3ec114311d1cea9feff01090c2e0430b
BAHTI <salah-eddine.bahti@capgemini.com>
2018-06-07 14:03:57 +0000

    app module merge

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,13 +14,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SignUpComponent } from './auth/sign-up/sign-up.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { LayoutModule } from '@angular/cdk/layout';
-<<<<<<< HEAD
 import { MatToolbarModule, MatButtonModule, MatSidenavModule,
  MatIconModule, MatListModule, MatCardModule, MatDatepickerModule,
 MatNativeDateModule, MatRadioModule, MatSelectModule, MatOptionModule,
 MatSlideToggleModule, MatTooltipModule, MatSnackBarModule } from '@angular/material';
-=======
->>>>>>> a1d5c5099bc13b93dc71ec15e055967864578328
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatTableModule } from '@angular/material/table';
@@ -28,24 +25,9 @@ import { MatMenuModule } from '@angular/material/menu';
 import { FormDemandeComponent } from './form-demande/form-demande.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { NavbarService } from './services/navbar/navbar.service';
-<<<<<<< HEAD
 import { FromCollaborateurComponent } from './from-collaborateur/from-collaborateur.component';
 import { CollaborateurService } from './services/collaborateur.service';
 import { FlexLayoutModule } from '@angular/flex-layout';
-=======
-import { Routes } from '@angular/router';
-import { MatButtonModule,MatIconModule,MatToolbarModule,MatListModule, MatCardModule, MatDatepickerModule, MatNativeDateModule, MatSnackBarModule, MatTooltipModule } from '@angular/material';
-
-
-const appRoutes: Routes = [
-  { path: 'listdemandes', component: DemandeComponent },
-  { path: 'accueil', component: HomeComponent },
-  { path: 'login', component: SignInComponent },
-  { path: 'listvals', component: ValidateurComponent },
-  { path: 'listcollabs', component: CollaborateurComponent },
-];
-
->>>>>>> a1d5c5099bc13b93dc71ec15e055967864578328
 @NgModule({
   declarations: [
     AppComponent,
@@ -86,14 +68,11 @@ const appRoutes: Routes = [
     MatSnackBarModule,
     MatTooltipModule,
     FormsModule,
-<<<<<<< HEAD
     ReactiveFormsModule,
     MatTooltipModule,
-    FlexLayoutModule
-=======
+    FlexLayoutModule,
     ReactiveFormsModule
 
->>>>>>> a1d5c5099bc13b93dc71ec15e055967864578328
   ],
   providers: [
     DemandeService,
